951faee97c87760640e2a61c1953a81f5dd15631
issaymk2 <issaymk2@gmail.com>
2011-06-29 11:54:54 +0000

    Use the C code of Snappy. (Backport the code in Hadoop trunk)

--- src/main/native/src/org_apache_hadoop.h
+++ src/main/native/src/org_apache_hadoop.h
@@ -1,4 +1,4 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -18,24 +18,52 @@
 
 /**
  * This file includes some common utilities 
+ * for all native code used in hadoop.
  */
  
-#if !defined HADOOP_SNAPPY_H
-#define HADOOP_SNAPPY_H
+#if !defined ORG_APACHE_HADOOP_H
+#define ORG_APACHE_HADOOP_H
 
-#include <config.h>
-#include <dlfcn.h>
-#include <jni.h>
+#if defined HAVE_CONFIG_H
+  #include <config.h>
+#endif
 
+#if defined HAVE_DLFCN_H
+  #include <dlfcn.h>
+#else
+  #error "dlfcn.h not found"
+#endif  
+
+#if defined HAVE_JNI_H    
+  #include <jni.h>
+#else
+  #error 'jni.h not found'
+#endif
 
 /* A helper macro to 'throw' a java exception. */ 
 #define THROW(env, exception_name, message) \
   { \
-	jclass ecls = env->FindClass(exception_name); \
+	jclass ecls = (*env)->FindClass(env, exception_name); \
 	if (ecls) { \
-	  env->ThrowNew(ecls, message); \
-	  env->DeleteLocalRef(ecls); \
+	  (*env)->ThrowNew(env, ecls, message); \
+	  (*env)->DeleteLocalRef(env, ecls); \
 	} \
+  }
+
+/* Helper macro to return if an exception is pending */
+#define PASS_EXCEPTIONS(env) \
+  { \
+    if ((*env)->ExceptionCheck(env)) return; \
+  }
+
+#define PASS_EXCEPTIONS_GOTO(env, target) \
+  { \
+    if ((*env)->ExceptionCheck(env)) goto target; \
+  }
+
+#define PASS_EXCEPTIONS_RET(env, ret) \
+  { \
+    if ((*env)->ExceptionCheck(env)) return (ret); \
   }
 
 /** 
@@ -47,16 +75,36 @@
  * @return returns the address where the symbol is loaded in memory, 
  *         <code>NULL</code> on error.
  */
+static __attribute__ ((unused))
+void *do_dlsym(JNIEnv *env, void *handle, const char *symbol) {
+  if (!env || !handle || !symbol) {
+  	THROW(env, "java/lang/InternalError", NULL);
+  	return NULL;
+  }
+  char *error = NULL;
+  void *func_ptr = dlsym(handle, symbol);
+  if ((error = dlerror()) != NULL) {
+  	THROW(env, "java/lang/UnsatisfiedLinkError", symbol);
+  	return NULL;
+  }
+  return func_ptr;
+}
+
+/* A helper macro to dlsym the requisite dynamic symbol and bail-out on error. */
+#define LOAD_DYNAMIC_SYMBOL(func_ptr, env, handle, symbol) \
+  if ((func_ptr = do_dlsym(env, handle, symbol)) == NULL) { \
+    return; \
+  }
 
 #define LOCK_CLASS(env, clazz, classname) \
-  if (env->MonitorEnter(clazz) != 0) { \
+  if ((*env)->MonitorEnter(env, clazz) != 0) { \
     char exception_msg[128]; \
     snprintf(exception_msg, 128, "Failed to lock %s", classname); \
     THROW(env, "java/lang/InternalError", exception_msg); \
   }
 
 #define UNLOCK_CLASS(env, clazz, classname) \
-  if (env->MonitorExit(clazz) != 0) { \
+  if ((*env)->MonitorExit(env, clazz) != 0) { \
     char exception_msg[128]; \
     snprintf(exception_msg, 128, "Failed to unlock %s", classname); \
     THROW(env, "java/lang/InternalError", exception_msg); \
@@ -64,3 +112,4 @@
 
 #endif
 
+//vim: sw=2: ts=2: et
